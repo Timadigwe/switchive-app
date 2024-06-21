4cc01bc0514ab8b575c5706a1c03437efc008498
timadigwe <timothyadigwe2019@gmail.com>
2024-06-21 12:05:13 +0100

    responsive ui fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${
+      className={`flex min-h-screen flex-col items-center justify-between p-5 lg:p-24 ${
         darkMode ? '' : 'bg-gray-100'
       }`}>
       <div
